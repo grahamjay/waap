bfc4785a943aac8b924110bad12f698ba9560f56
eGrote <g12b3@apex-prog-16949.local>
2015-12-01 13:25:32 -0600

    New File

--- Weightroom001.php
+++ Weightroom001.php
@@ -1,50 +1,52 @@
-<!-- this is the first initial log in page -->
-
-
-
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="weightroom001.css">
-
+<style>
+   h1 {
+       color:rgb(254, 255, 0)
+   }
+   #number {
+       color:rgb(254, 255, 0)
+   }
+</style>
 </head>
+<body style="background-image:url(weightroom.jpg); background-size:100%;background-repeat:no-repeat; text-align:center; padding-right:5%">
 
-<h1> Waukee Weight Room Login
-</h1>
 <form id="number" action="Weightroom001.php" method="POST">
-	<p>ID number:</p>
-	
-	
-	<textarea rows="1" class="username" id="Student ID" name="id" placeholder="    Enter ID Number"></textarea>
-
-	<br><br>
-	<button type="button" onclick="window.location.href='Weightroom002.php'">Submit</button>
-</form>
+   <div style="font-size: 60px; padding-top:40%">ID number:</div>
+   <textarea style="font-size:29px"rows="1" id="id" name="id" placeholder="Enter ID Number"></textarea><br>
+   <button type="submit" name="ID" style="width:100px; height:50px; font-size:20px">Submit</button>
 
 <?php
 
 if ($_POST['id'] == 123456) {
-	header("Location: http://107.170.184.216/admin.php");
-    exit();
-    }
-if(isset($_POST['id'])){
-	$sql = " SELECT * FROM athletic_users WHERE id='".$_POST['id']."'";
-	$db = new mysqli("127.0.0.1", "root", "root", "test");
-	$result = $db->query($sql);
-	$row = $result->fetch_assoc();
-	if ($_POST['id'] == $row['id']) {
-		$_SESSION['cat'] = $_POST['id'];
-		setcookie('userid', $_POST['id'], time() + 3600);
-		header("Location: http://107.170.184.216/Weightroom002.php");
-	} else {
-		echo "<br><br><br>";
-		echo "User not found, please try again.";
-	}
-}
+   header("Location: http://localhost/admin.php");
+   exit();
+   }
 
-echo var_dump($_POST);
+if(isset($_POST['id'])){
+
+   $sql = "
+   SELECT * FROM athletic_users WHERE id='".$_POST['id']."'";
+
+   $db = new mysqli("127.0.0.1", "root", "", "test", 3307);
+   $result = $db->query($sql);
+
+   $row = $result->fetch_assoc();
+
+
+   if ($_POST['id'] == $row['id']) {
+       $_SESSION['cat'] = $_POST['id'];
+       setcookie('userid', $_POST['id'], time() + 3600);
+       header("Location: http://localhost/weightroom002.php");
+
+   } else {
+       echo "<br><br><br>";
+       echo "User not found, please try again.";
+   }
+}
 ?>
 
-
+</form>
 </body>
 </html>
