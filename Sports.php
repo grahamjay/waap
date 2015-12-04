2c18369670e3e76b9ef2e11a63cf8c4126f0f48e
palmsco <palmsco@gmail.com>
2015-12-03 21:44:23 -0600

    I think I finished it

--- Sports.php
+++ Sports.php
@@ -24,51 +24,191 @@
 
 		
 	<br><br>
-<select>
+	
+	
+
+<select name="sport">
+
 
   <option value="Nada">SELECT INDV SPORT</option>
-  <option value="FBL">Football</option>
-  <option value="VBL">Volleyball</option>
-  <option value="BXC">Boys Cross Country</option>
-  <option value="GXC">Girls Cross Country</option>
-  <option value="CHR">Cheer</option>
-  <option value="DNC">Dance</option>
-  <option value="BGL">Boys Golf</option>
-  <option value="GBB">Girls Basketball</option>
-  <option value="BBB">Boys Basketball</option>
-  <option value="GBL">Girls Bowling</option>
-  <option value="BBL">Boys Bowling</option>
-  <option value="WRS">Wrestling</option>
-  <option value="GGL">Girls Golf</option>
-  <option value="BTF">Boys Track & Field</option>
-  <option value="GTF">Girls Track & Field</option>
-  <option value="BTS">Boys Tennis</option>
-  <option value="GTS">Girls Tennis</option>
-  <option value="BSC">Boys Soccer</option>
-  <option value="GSC">Girls Soccer</option>
-  <option value="SFB">Softball</option>
-  <option value="BSB">Baseball</option>
+  <option value="1">Football</option>
+  <option value="2">Volleyball</option>
+  <option value="15">Boys Cross Country</option>
+  <option value="16">Girls Cross Country</option>
+  <option value="22">Cheer</option>
+  <option value="23">Dance</option>
+  <option value="13">Boys Golf</option>
+  <option value="6">Girls Basketball</option>
+  <option value="5">Boys Basketball</option>
+  <option value="10">Girls Bowling</option>
+  <option value="9">Boys Bowling</option>
+  <option value="21">Wrestling</option>
+  <option value="14">Girls Golf</option>
+  <option value="17">Boys Track & Field</option>
+  <option value="18">Girls Track & Field</option>
+  <option value="7">Boys Tennis</option>
+  <option value="8">Girls Tennis</option>
+  <option value="19">Boys Soccer</option>
+  <option value="20">Girls Soccer</option>
+  <option value="4">Softball</option>
+  <option value="3">Baseball</option>
 </select>
 <br><br>
 <input type = "submit" name="submit" value="Insert Selected Sport">
 
-<br><br><br>
 
 <?php
-    if(isset($_POST['submit'])){
-    echo "<br><br>";
-        $db = new mysqli("127.0.0.1", "root", "", "test", 3307);
-		$result2 = $db->query("INSERT INTO statement");
+        $db = new mysqli("127.0.0.1", "root", "root", "test");
+	if(isset($_POST['studentid1']) and isset($_POST['sport']) and $_POST['sport']!= 'Nada')
+	{
+	    
+		$result = $db->query("INSERT into sports_enrollment (student_id, sport_id) values (".$_POST['studentid1'].",". $_POST['sport'].");");
 
-I NEED TO WRITE A BUNCH OF IF STATEMENTS IN HERE THAT WILL TAKE THE INPUT FROM ABOVE AND INSERT THEM INTO THE SQL TABLE
-THAT WILL REQUIRE A NEW IF FOR EACH INPUT, WITH THE ELSE BEING TO RERUN THE PAGE WITH A SUMMARY OF WHO THEY ENTERED FOR EACH SPORT						
+	
+	}
+	if(isset($_POST['studentid2']) and isset($_POST['sport']) and $_POST['sport']!= 'Nada')
+	{
+	
+		$result = $db->query("INSERT into sports_enrollment (student_id, sport_id) values (".$_POST['studentid2'].",". $_POST['sport'].");");
 
-}}
+	
+	}
 
+	if(isset($_POST['studentid3']) and isset($_POST['sport']) and $_POST['sport']!= 'Nada')
+	{
 
+		$result = $db->query("INSERT into sports_enrollment (student_id, sport_id) values (".$_POST['studentid3'].",". $_POST['sport'].");");
 
+	
+	}
+	if(isset($_POST['studentid4']) and isset($_POST['sport']) and $_POST['sport']!= 'Nada')
+	{
+	
+		$result = $db->query("INSERT into sports_enrollment (student_id, sport_id) values (".$_POST['studentid4'].",". $_POST['sport'].");");
+
+	
+	}
+	
+		if(isset($_POST['studentid5']) and isset($_POST['sport']) and $_POST['sport']!= 'Nada')
+	{
+		$result = $db->query("INSERT into sports_enrollment (student_id, sport_id) values (".$_POST['studentid5'].",". $_POST['sport'].");");
+
+	
+	}
+	
+		if(isset($_POST['studentid6']) and isset($_POST['sport']) and $_POST['sport']!= 'Nada')
+	{
+		$result = $db->query("INSERT into sports_enrollment (student_id, sport_id) values (".$_POST['studentid6'].",". $_POST['sport'].");");
+
+	
+	}
+	
+		if(isset($_POST['studentid7']) and isset($_POST['sport']) and $_POST['sport']!= 'Nada')
+	{
+		$result = $db->query("INSERT into sports_enrollment (student_id, sport_id) values (".$_POST['studentid7'].",". $_POST['sport'].");");
+
+	
+	}
+		if(isset($_POST['studentid8']) and isset($_POST['sport']) and $_POST['sport']!= 'Nada')
+	{
+		$result = $db->query("INSERT into sports_enrollment (student_id, sport_id) values (".$_POST['studentid8'].",". $_POST['sport'].");");
+
+	
+	}
+	
+		if(isset($_POST['studentid9']) and isset($_POST['sport']) and $_POST['sport']!= 'Nada')
+	{
+		$result = $db->query("INSERT into sports_enrollment (student_id, sport_id) values (".$_POST['studentid9'].",". $_POST['sport'].");");
+
+	
+	}
+	
+		if(isset($_POST['studentid10']) and isset($_POST['sport']) and $_POST['sport']!= 'Nada')
+	{
+		$result = $db->query("INSERT into sports_enrollment (student_id, sport_id) values (".$_POST['studentid10'].",". $_POST['sport'].");");
+
+	
+	}
 ?>
 </form>
 
 
+<hr>
+
+
+
+<form id="indv_sports" action="sports.php" method="POST">
+
+<h1>View attendance for an individual sport: </h1>
+<select name="indv_sport">
+
+
+  <option value="Nada">SELECT INDV SPORT</option>
+  <option value="1">Football</option>
+  <option value="2">Volleyball</option>
+  <option value="15">Boys Cross Country</option>
+  <option value="16">Girls Cross Country</option>
+  <option value="22">Cheer</option>
+  <option value="23">Dance</option>
+  <option value="13">Boys Golf</option>
+  <option value="6">Girls Basketball</option>
+  <option value="5">Boys Basketball</option>
+  <option value="10">Girls Bowling</option>
+  <option value="9">Boys Bowling</option>
+  <option value="21">Wrestling</option>
+  <option value="14">Girls Golf</option>
+  <option value="17">Boys Track & Field</option>
+  <option value="18">Girls Track & Field</option>
+  <option value="7">Boys Tennis</option>
+  <option value="8">Girls Tennis</option>
+  <option value="19">Boys Soccer</option>
+  <option value="20">Girls Soccer</option>
+  <option value="4">Softball</option>
+  <option value="3">Baseball</option>
+</select>
+<br><br>
+<input type = "submit" name="submit" value="View Selected Sport">
+
+<br><br><br>
+<?php
+ 
+	if(isset($_POST['indv_sport']) and $_POST['sport']!= 'Nada')
+	{
+	    echo "<br><br>";
+		$result = $db->query("SELECT grade, lastname, firstname, sport_name, count(user_id) FROM sports_enrollment JOIN sports ON sports_enrollment.sport_id = sports.id
+								JOIN athletic_users ON sports_enrollment.student_id = athletic_users.id
+                                JOIN athletic_attendance ON athletic_users.id=athletic_attendance.user_id
+								WHERE sports.id=".$_POST['indv_sport']."
+                                GROUP BY user_id
+                                ORDER BY lastname;");
+
+echo "<table border = 1>";
+echo "<tr>";
+echo "<td> Grade </td>";
+echo "<td> Last Name </td>";
+echo "<td>First Name </td>";
+echo "<td> Sport </td>";
+echo "<td> Days Attended </td>";
+
+while ($row = $result->fetch_assoc()){
+   echo "<tr>";
+   echo "<td>".htmlentities($row['grade'])."</td>";
+   echo "<td>".htmlentities($row['lastname'])."</td>";
+   echo "<td>".htmlentities($row['firstname'])."</td>";
+   echo "<td>".htmlentities($row['sport_name'])."</td>";
+   echo "<td>".htmlentities($row['count(user_id)'])."</td>";
+  echo "</tr>";
+
+
+}echo "</table>";
+
+	}
+
+
+?>
+
+</form>
+<a href="admin.php"><h1>Click Here to go back to ADMIN page</h1></a>
+
 </body>
+
