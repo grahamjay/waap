0b05532af4d4301c2a8ad5a7743cf28141de76fc
palmsco <palmsco@gmail.com>
2015-12-08 13:25:43 -0600

    fixed

--- admin.php
+++ admin.php
@@ -276,7 +276,7 @@ Or you can search for an individual student:
     if(isset($_POST['firstnamebutton'])){
     echo "<br><br>";
         $db = new mysqli("127.0.0.1", "root", "root", "test");
-		$result3 = $db->query("SELECT grade, lastname, firstname, count(user_id) from athletic_attendance 
+		$result3 = $db->query("SELECT id, grade, lastname, firstname, count(user_id) from athletic_attendance 
 									join athletic_users
     								on athletic_attendance.user_id=athletic_users.id
     								where lastname = ('".$_POST['lastnamebutton']."' and firstname like '%".$_POST['firstname']."%') 
@@ -342,7 +342,7 @@ echo "</table>";
     if(isset($_POST['idnumberbutton'])){
     echo "<br><br>";
         $db = new mysqli("127.0.0.1", "root", "root", "test");
-		$result4 = $db->query("SELECT grade, lastname, firstname, count(user_id) from athletic_attendance
+		$result4 = $db->query("SELECT id, grade, lastname, firstname, count(user_id) from athletic_attendance
 								JOIN athletic_users
     							ON athletic_attendance.user_id=athletic_users.id
     							WHERE id = '".$_POST['IDnumber']."';");
