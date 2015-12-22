fd90d797a595128bc622e6c2db3d9391817a40ab
palmsco <palmsco@gmail.com>
2015-12-22 11:12:07 -0600

    all sorts of changes

--- Weightroom002.php
+++ Weightroom002.php
@@ -1,5 +1,5 @@
 <!--This is the second page that you select either check in or information-->
-
+<link rel="stylesheet" type="text/css" href="bootstrap.css">
 <?php
 session_start();
 $sql = "
@@ -16,7 +16,7 @@ if ($_COOKIE['userid'] == $row['id']) {
 
 } else {
 	echo "User not found, please go back and try again.";
-	
+
 }
 
 
@@ -24,12 +24,12 @@ if ($_COOKIE['userid'] == $row['id']) {
 
 <html>
 <head>
-<style>
-	h1 {
-		color:rgb(148,0,211)
-	}
-<title>Check in/ User info</title>
-</style>
+	<style>
+		h1 {
+			color:rgb(148,0,211)
+		}
+		<title>Check in/ User info</title>
+	</style>
 </head>
 
 <body style="background-color:gold">
@@ -38,10 +38,9 @@ if ($_COOKIE['userid'] == $row['id']) {
 
 <form style="text-align: center; height: 15em; background-repeat:no-repeat;background-position: center">
 
-<input type="button" value="Check In" onclick="window.location.href='in.php'" /><br><br>
 
-<input type="button" value="General Info" onclick="window.location.href='generalinfo.php'" />
-
+	<input type="button" value="Check In" style="width:200px; height:100px; font size:40px" onclick="window.location.href='in.php'" /><br><br>
+	<input type="button" value="General Info" onclick="window.location.href='generalinfo.php'" />
 
 
 </form>
